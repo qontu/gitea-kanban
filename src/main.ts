65276c9cd1ff7ee81ebc8074ce95c910a94bfb12
xxxtonixxx <tonivj5@gmail.com>
2018-09-26 17:27:28 +0200

    fix: Add fetch polyfill to work in older browsers

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import VueKanban from "vue-kanban";
 import VueRouter from "vue-router";
 import Vuesax from "vuesax";
 
+import "whatwg-fetch";
+
 import "vuesax/dist/vuesax.css";
 import "material-icons/iconfont/material-icons.css";
 
